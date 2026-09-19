05f9e11084425220228bf0e4d848307080a45172
sjaimespro <144156374+sjaimespro@users.noreply.github.com>
2026-09-19 17:21:35 -0500

    Update datos-catalogo.js

--- datos-catalogo.js
+++ datos-catalogo.js
@@ -62,10 +62,11 @@ var CATALOGO = {
       banner: {
         foto:'imgs/yute-banner.jpg',
         titulo:'Balerina Tejido con Moño',
+          precio: 40.00,
         descripcion:'Cuerpo tejido tipo yute con moño de charol y correa de hebilla. Disponible en:',
         colores:['Negro','Vino','Camel'],
         alt:'Balerinas tejidas tipo yute con moño, en negro, vino y camel'
-         precio: 38.00,
+      
       }
     },
     { id:'encaje', nombre:'Encaje', descripcion:'Delicadas y románticas, ideales para ocasiones especiales' },
